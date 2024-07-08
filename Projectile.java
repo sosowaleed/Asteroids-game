9286f66b628cd3c24b524d39a1cc6d0ae1c4e879
sosowaleed <71035541+sosowaleed@users.noreply.github.com>
2024-07-08 12:57:33 +0300

    Add files via upload

---
+++ Projectile.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package asteroids;
+
+/**
+ *
+ * @author walee
+ */
+import javafx.scene.shape.Polygon;
+
+public class Projectile extends Character {
+    
+    public Projectile(int x, int y) {
+        super(new Polygon(0, 0, 5, -2, 5, 5, -2, 2), x, y);
+    }
+}
